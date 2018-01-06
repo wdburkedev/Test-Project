48dde453f45607db667644dccd7f685bfd585dd9
Bill Burke <wdburkedev@gmail.com>
2018-01-06 05:35:46 -0600

    Initial Commit

---
+++ Test Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Project
+//
+//  Created by Bill Burke on 1/6/18.
+//  Copyright © 2018 Bill Burke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
